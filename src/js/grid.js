c881cb9e7e960966ee59741832353ad15ec0a7ca
Daniel Cook <dan.c.cook@gmail.com>
2025-06-24 18:42:16 -0400

    Tidy whitespace

--- src/js/grid.js
+++ src/js/grid.js
@@ -128,6 +128,7 @@ class Grid {
             case DIRECTIONS.horizontal:
                 this.#direction = DIRECTIONS.vertical;
                 break;
+                
             case DIRECTIONS.vertical:
                 this.#direction = DIRECTIONS.horizontal;
                 break;
@@ -182,12 +183,9 @@ class Grid {
 
         this.disableKeys();
 
-        
-
         this.#currentCell = this.#cells[j][i];
 
         this.enableKeys();
-
 
     }
 

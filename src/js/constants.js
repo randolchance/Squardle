1ba17136964e336dc2c5af4d65e19b817ba9f1c9
Daniel Cook <dan.c.cook@gmail.com>
2025-06-30 20:36:19 -0400

    Update enums, add Mode enum

--- src/js/constants.js
+++ src/js/constants.js
@@ -11,6 +11,8 @@ export const NON_CHARACTER_KEYS = [
     'Backspace',
 ];
 
+export const VALID_KEYS = CHARACTER_KEYS.concat(NON_CHARACTER_KEYS);
+
 export const DIRECTIONS = {
     horizontal: 0,
     vertical: 1,
@@ -19,7 +21,13 @@ export const DIRECTIONS = {
 export const HINTS = {
     unused: 0,
     inWord: 1,
-    inWordHorizontal: 2,
-    inWordVertical: 3,
-    correct: 4,
+    inWordHorizontal: 3,
+    inWordVertical: 5,
+    inBoth: 7,
+    correct: 8,
+}
+
+export const MODES = {
+    easy: 0,
+    normal: 1,
 }

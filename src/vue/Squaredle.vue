31ef72402b9cbe04286b51a7277bdc569be3d2a0
Daniel Cook <dan.c.cook@gmail.com>
2025-07-08 08:08:47 -0400

    Round up pixel size

--- src/vue/Squaredle.vue
+++ src/vue/Squaredle.vue
@@ -35,7 +35,7 @@ function changeWord( word_index ) {
 <style>
 
 :root {
-    --pixel-size: round(down, 0.73vw, 1px);
+    --pixel-size: round(up, 0.73vw, 1px);
 
     --pixel-size-x: var(--pixel-size);
     --pixel-size-y: var(--pixel-size);

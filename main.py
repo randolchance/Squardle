6ab9b24ce05ce1ac7f69e6e6e47f2df2f3a42dcf
Daniel Cook <dan.c.cook@gmail.com>
2025-07-13 17:06:59 -0400

    Update cors and how session handles scope and data

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ from fastapi.templating import Jinja2Templates as Templates
 from starlette.config import Config
 from starlette.datastructures import Secret
 from starlette.middleware.sessions import SessionMiddleware
+from pydantic import BaseModel
 
 from puzzle_master import PuzzleMaster
+
+
+class GuessData(BaseModel):
+    word: str
+    word_index: int
 
 config = Config("./config.env")
 
@@ -18,14 +24,14 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://localhost:5173",
+    "http://localhost:8000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET","POST","OPTIONS"],
     allow_headers=["*"],
 )
 
@@ -34,9 +40,9 @@ app.add_middleware(
     secret_key=str(SECRET_KEY),
     session_cookie="Squardle",
     max_age=SESSION_EXPIRY,
-    same_site="strict",
+    same_site="Lax",
     path="/p",
-    #https_only=True,
+    https_only=False,
 )
 
 
@@ -45,7 +51,7 @@ templates = Templates(directory="dist")
 
 app.mount("/assets", StaticFiles(directory="dist/assets"), name="static")
 
-@app.get("/")
+@app.get("/p")
 async def index(request: Request):
     
     response = templates.TemplateResponse("index.html",{"request": request})
@@ -58,10 +64,10 @@ async def query_words(q: str):
     return words.getWords(q)
 
 @app.get("/p/{p}/start")
-async def start_puzzle(request: Request, p: int):
-    puzzle_data = PuzzleMaster.initialiseNewPuzzle(p)
+async def start_puzzle(request: Request, p:int, m: int):
+    puzzle_data = PuzzleMaster.initialiseNewPuzzle(p, m)
 
-    request.session['current_puzzle'] = puzzle_data
+    request.session.update({'current_puzzle': puzzle_data})
     
     response = JSONResponse(status_code=status.HTTP_200_OK, content={
         'current_puzzle': puzzle_data
@@ -77,15 +83,28 @@ async def test(request: Request):
     return response
 
 
-@app.get("/p/guess")
-async def guess_word(p: int, i: int, word: str, m: int):
-    puzzleMaster = PuzzleMaster()
+@app.post("/p/guess")
+async def guess_word(request: Request, guessData: GuessData):
+    word, word_index = guessData.model_dump().values()
+
     if (not word.isascii()):
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST)
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+    
+    current_puzzle_data = request.session.get('current_puzzle')
+    if not current_puzzle_data:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+    
+    # Get current state of the current_puzzle from the session
+    p = current_puzzle_data['p']
+    mode = current_puzzle_data['m']
+    word_size = current_puzzle_data['s']
+    current_puzzle_state = current_puzzle_data['data']
+
+    puzzleMaster = PuzzleMaster(word_size)
 
     if (not puzzleMaster.isValidWord(word)):
         return JSONResponse(status_code=status.HTTP_200_OK, content=None)
 
-    easy_mode = not bool(m)
     hints = puzzleMaster.guess(p, i, word, easy_mode)
+    easy_mode = mode == PuzzleMaster.Modes.ALL_HINTS
     return JSONResponse(status_code=status.HTTP_200_OK, content=hints)

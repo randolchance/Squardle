10981d053858d20ad219733ec4f551f972c9cfb2
Daniel Cook <dan.c.cook@gmail.com>
2025-07-23 17:52:50 -0400

    Unused

--- src/vue/Cell.vue
+++ src/vue/Cell.vue
@@ -28,10 +28,6 @@ const { cell } = defineProps({
     padding: var(--pixel-size-y) var(--pixel-size-x) var(--pixel-size-y) var(--pixel-size-x);
 }
 
-.cell-letter {
-    --scale: 1;
-}
-
 .cell.row-selected-start {
     border-style: var(--selected-border-style) none var(--selected-border-style) var(--selected-border-style);
     padding: 0 var(--pixel-size-x) 0 0;
